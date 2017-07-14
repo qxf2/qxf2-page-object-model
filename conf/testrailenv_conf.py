9a3799562f2aeb0128ec105ff54976f270e904d4
rohanj-qxf2 <rohan.j@qxf2.com>
2017-07-14 12:46:11 +0530

    Fixed error

--- conf/testrailenv_conf.py
+++ conf/testrailenv_conf.py
@@ -2,6 +2,6 @@
 Conf file to hold the TestRail url and credentials
 """
 
-TestRail_url = "Add your TestRail url"
-TestRail_user = "Add your TestRail username"
-TestRail_password = "Add your TestRail password"
+testrail_url = "Add your testrail url"
+testrail_user = "Add your testrail username"
+testrail_password = "Add your testrail password"
