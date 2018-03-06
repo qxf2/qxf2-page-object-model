3d7822da480cb4091fe12458ba36a59a4adeafb0
rohan dudam <rohan@qxf2.com>
2018-03-06 18:28:08 +0530

    Added 2 test case id's for mobile_bitcoin_price_test

--- conf/testrail_caseid_conf.py
+++ conf/testrail_caseid_conf.py
@@ -9,4 +9,6 @@ test_example_form_email = 128
 test_example_form_phone = 129
 test_example_form_gender = 130
 test_example_form_footer_contact = 131
+test_bitcoin_price_page_header = 234
+test_bitcoin_real_time_price = 235
 
