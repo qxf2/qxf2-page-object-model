8343351ee2f6e6c2e25c9a73c85fc39bf8721e5b
Indira Nellutla <indira@qxf2.com>
2018-08-06 12:58:59 +0530

    Modified the code, removed the code from main method and created a method generate_xpath as part of the class and calling it from main

--- utils/xpath_util.py
+++ utils/xpath_util.py
@@ -5,23 +5,61 @@ Qxf2 Services: Utility script to generate XPaths for the given URL
 * Find all Input and Button tags
 * Guess the XPaths
 * To run the script in Gitbash use command 'python -u utils/xpath_util.py'
+
 """
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class Xpath_Util:
-    "Class to generate the xpaths"  
+    "Class to generate the xpaths"
+
+    def __init__(self):
+        self.elements = None
+        self.guessable_elements = ['input','button']
+        self.known_attribute_list = ['id','name','placeholder','value','type','title','class']
+
+    def generate_xpath(self,soup):
+        "generate the xpath"  
+        result_flag = False
+        try:
+            for guessable_element in self.guessable_elements:
+                self.elements = soup.find_all(guessable_element)                                
+                for element in self.elements:                                   
+                    if (not element.has_attr("type")) or (element.has_attr("type") and element['type'] != "hidden"):
+                        for attr in self.known_attribute_list:                                                      
+                            if element.has_attr(attr):                                                             
+                                locator = self.guess_xpath(guessable_element,attr,element)                               
+                                if len(driver.find_elements_by_xpath(locator))==1:
+                                    result_flag = True                                   
+                                    print locator.encode('utf-8')
+                                    break
+                            elif guessable_element == 'button' and element.getText():                                                                  
+                                button_text = element.getText()
+                                if element.getText() == button_text.strip():
+                                    locator = xpath_obj.guess_xpath_button(guessable_element,"text()",element.getText())
+                                else:
+                                    locator = xpath_obj.guess_xpath_using_contains(guessable_element,"text()",button_text.strip())
+                                if len(driver.find_elements_by_xpath(locator))==1:
+                                    result_flag = True                                   
+                                    print locator.encode('utf-8')
+                                    break        
+        except Exception,e:
+            print "Exception when trying to generate xpath for:%s"%guessable_element
+            print "Python says:%s"%str(e)                                                  
+    
+        return result_flag
+
     def guess_xpath(self,tag,attr,element):
         "Guess the xpath based on the tag,attr,element[attr]"
         #Class attribute returned as a unicodeded list, so removing 'u from the list and joining back
         if type(element[attr]) is list:
             element[attr] = [i.encode('utf-8') for i in element[attr]]
-            element[attr] = ' '.join(element[attr])
+            element[attr] = ' '.join(element[attr])                             
         self.xpath = "//%s[@%s='%s']"%(tag,attr,element[attr])
-                
-        return  self.xpath
-
+                                     
+        return  self.xpath            
+    
     def guess_xpath_button(self,tag,attr,element):
         "Guess the xpath for button tag"
         self.button_xpath = "//%s[%s='%s']"%(tag,attr,element)
@@ -33,16 +71,8 @@ class Xpath_Util:
         self.button_contains_xpath = "//%s[contains(%s,'%s')]"%(tag,attr,element)
         
         return self.button_contains_xpath
-    
-    def get_elements_list(self,locator):
-        "Get all the elements for the given xpath"
-        self.ifunique = False
-        if len(driver.find_elements_by_xpath(locator))==1:
-            self.ifunique = True
-            
-        return self.ifunique
-
-    
+   
+   
 #-------START OF SCRIPT--------
 if __name__ == "__main__":
     print "Start of %s"%__file__
@@ -60,90 +90,9 @@ if __name__ == "__main__":
     #Parsing the HTML page with BeautifulSoup
     page = driver.execute_script("return document.body.innerHTML").encode('utf-8') #returns the inner HTML as a string
     soup = BeautifulSoup(page, 'html.parser')
- 
-    #Finding all input tags 
-    inputs =  soup.find_all('input')
     
-    #Finding all button tags
-    buttons = soup.find_all('button')
-    
-    if inputs == [] and buttons == []:
-        print "No input or button tags available to generate the XPath"
-    
-    for input in inputs:        
-        try:
-            if input.has_attr('type'):
-                if input['type'] == "hidden":
-                    continue                
-            if input.has_attr("id"):               
-                locator = xpath_obj.guess_xpath("input","id",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue                                          
-            if input.has_attr("name"):               
-                locator = xpath_obj.guess_xpath("input","name",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue
-            if input.has_attr("placeholder"):                
-                locator = xpath_obj.guess_xpath("input","placeholder",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue          
-            if input.has_attr("value"):                
-                locator = xpath_obj.guess_xpath("input","value",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue 
-            if input.has_attr("type"):                
-                locator = xpath_obj.guess_xpath("input","type",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue                                    
-            if input.has_attr("class"):                
-                locator = xpath_obj.guess_xpath("input","class",input)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue
-        except Exception,e:
-            print "Exception when generating XPath for input tags:%s"%__file__
-            print "Python says:%s"%str(e)
-            
-    for button in buttons:
-        try:
-            if button.has_attr("id"):
-                locator = xpath_obj.guess_xpath("button","id",button)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue           
-            if button.getText():
-                button_text = button.getText()
-                if button.getText() == button_text.strip():
-                    locator = xpath_obj.guess_xpath_button("button","text()",button.getText())
-                else:
-                    locator = xpath_obj.guess_xpath_using_contains("button","text()",button_text.strip())
-
-                if xpath_obj.get_elements_list(locator):   
-                    #To fix UnicodeEncodeError: 'ascii' codec can't encode character u'\xa0' in position 20: ordinal not in range            
-                    print locator.encode('utf-8')
-                    continue
-            if button.has_attr("name"):            
-                locator = xpath_obj.guess_xpath("button","name",button)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue 
-            if button.has_attr("type"):           
-                locator = xpath_obj.guess_xpath("button","type",button)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue 
-            if button.has_attr("title"):                
-                locator = xpath_obj.guess_xpath("button","title",button)
-                if xpath_obj.get_elements_list(locator):
-                    print locator
-                    continue 
-        except Exception,e:
-            print "Exception when generating XPath for button tags:%s"%__file__
-            print "Python says:%s"%str(e)
-     
+    #execute generate_xpath
+    if xpath_obj.generate_xpath(soup) is False:
+        print "No XPaths generated for the URL:%s"%url
+           
     driver.quit() 
