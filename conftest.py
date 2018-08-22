da7e8fa97ef018a7ce3af43a1f46e9b5e0cdd7ae
nilaya1234 <nilaya@qxf2.com>
2018-08-22 11:30:21 +0530

    circleci python3 changes

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import pytest
 import os
-from .conf import browser_os_name_conf
-from .utils import post_test_reports_to_slack
-from .utils.email_pytest_report import Email_Pytest_Report
-from .utils import Tesults
+from conf import browser_os_name_conf
+from utils import post_test_reports_to_slack
+from utils.email_pytest_report import Email_Pytest_Report
+from utils import Tesults
 
 
 @pytest.fixture
