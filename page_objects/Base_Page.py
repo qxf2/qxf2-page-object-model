5775523a1ec73f653ebc9db4eba21f9eb0117728
Sravanti25 <sravanti.t25@gmail.com>
2019-07-18 20:20:36 +0530

    Added the switch_frame method

--- page_objects/Base_Page.py
+++ page_objects/Base_Page.py
@@ -293,7 +293,6 @@ class Base_Page(Borg,unittest.TestCase):
 
         return result_flag
 
-
     def close_current_window(self):
         "Close the current window"
         result_flag = False
@@ -320,7 +319,15 @@ class Base_Page(Borg,unittest.TestCase):
         "Get the current window handle"
         pass
 
-
+    def switch_frame(self,name=None,index=None,wait_time=2):
+        "switch to iframe"
+        self.wait(wait_time)
+        self.driver.switch_to.default_content()
+        if name is not None:
+            self.driver.switch_to.frame(name)
+        elif index is not None:
+            self.driver.switch_to.frame(self.driver.find_elements_by_tag_name("iframe")[index])
+        
     def _get_locator(key):
         "fetches locator from the locator conf"
         value = None
