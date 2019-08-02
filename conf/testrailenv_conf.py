ec242576585672d7e55b1e6288223b102ffb1496
nilaya1234 <nilaya@qxf2.com>
2019-08-02 12:25:14 +0530

    changed testrailenv to origin

--- conf/testrailenv_conf.py
+++ conf/testrailenv_conf.py
@@ -1,11 +1,8 @@
 """
 Conf file to hold the TestRail url and credentials
 """
-'''
+
 testrail_url = "Add your testrail url"
 testrail_user = "Add your testrail username"
 testrail_password = "Add your testrail password"
-'''
-testrail_url = "https://qxf2.testrail.com/"
-testrail_user = "avinash@qxf2.com"
-testrail_password = "GetTestRail"
+
