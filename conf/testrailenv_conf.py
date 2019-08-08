9bbe62c68504300e07204b6af8cc68c8279aa5c0
Annapoorani Bala <Arunkumar>
2019-08-08 12:09:53 +0530

    Added the comments line and removed the spaces

--- conf/testrailenv_conf.py
+++ conf/testrailenv_conf.py
@@ -5,4 +5,3 @@ Conf file to hold the TestRail url and credentials
 testrail_url = "Add your testrail url"
 testrail_user = "Add your testrail username"
 testrail_password = "Add your testrail password"
-
