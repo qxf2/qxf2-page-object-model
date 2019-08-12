a6054372c84f9da1a33b07ca429c560664c8b379
Rohini Gopal <rohini.gopal@qxf2.com>
2019-08-12 11:48:27 +0530

    Added one 2 line space before and after the method

--- page_objects/Base_Page.py
+++ page_objects/Base_Page.py
@@ -236,10 +236,12 @@ class Base_Page(Borg,unittest.TestCase):
         "Get the current URL"
         return self.driver.current_url
 
+        
     def get_page_title(self):
         "Get the current page title"
         return self.driver.title
     
+
     def get_page_paths(self,section):
         "Open configurations file,go to right sections,return section obj"
         pass
