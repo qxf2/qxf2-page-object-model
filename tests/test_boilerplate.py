48649b9de3387f0a5ada075d260bc1c67d047422
akkuldn <akkul.dn@qxf2.com>
2020-01-20 18:28:43 +0530

    Added test_boilerplate file to help get started using the framework

---
+++ tests/test_boilerplate.py
@@ -0,0 +1,61 @@
+"""
+This test file will help you get started in writing a new test using our framework
+"""
+import os,sys,time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from page_objects.PageFactory import PageFactory
+from utils.Option_Parser import Option_Parser
+import pytest
+
+
+@pytest.mark.GUI
+def test_boilerplate(test_obj):
+
+    "Run the test"
+    try:
+        #Initalize flags for tests summary
+        expected_pass = 0
+        actual_pass = -1
+        
+        #Set start_time with current time
+        start_time = int(time.time())	
+
+        #This is the test object, you can change it to the desired page with relevance to the page factory
+        test_obj = PageFactory.get_page_object("main page") 
+        
+
+        #13. Print out the result
+        test_obj.write_test_summary()
+        expected_pass = test_obj.result_counter
+        actual_pass = test_obj.pass_counter        
+
+    except Exception as e:
+        print("Exception when trying to run test: %s"%__file__)
+        print("Python says:%s"%str(e))
+    
+    assert expected_pass == actual_pass, "Test failed: %s"%__file__
+
+    
+#---START OF SCRIPT   
+if __name__=='__main__':
+    print("Start of %s"%__file__)
+    #Creating an instance of the class
+    options_obj = Option_Parser()
+    options = options_obj.get_options()
+                
+    #Run the test only if the options provided are valid
+    if options_obj.check_options(options): 
+        test_obj = PageFactory.get_page_object("Zero",base_url=options.url)
+
+        #Setup and register a driver
+        test_obj.register_driver(options.remote_flag,options.os_name,options.os_version,options.browser,options.browser_version,options.remote_project_name,options.remote_build_name)
+
+
+        test_boilerplate(test_obj)
+                
+        #teardowm
+        test_obj.wait(3)
+        test_obj.teardown() 
+    else:
+        print('ERROR: Received incorrect comand line input arguments')
+        print(option_obj.print_usage())
