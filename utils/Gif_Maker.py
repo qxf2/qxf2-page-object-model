1bbacd5362d9210d50944d458658ed6a8812aed1
kirancv4 <kiran.cv@qxf2.com>
2020-01-22 16:59:02 +0530

    Added condition to check if the images exists or not to make a gif.

--- utils/Gif_Maker.py
+++ utils/Gif_Maker.py
@@ -6,13 +6,18 @@ import imageio
 import os
 
 
+
+
 def make_gif(screenshot_dir_path,name = "test_recap",suffix=".gif",duration=2):
     "Creates gif of the screenshots"
     images = []
     filenames = os.listdir(screenshot_dir_path)
     gif_name = os.path.join(screenshot_dir_path, name + suffix)
-    
-    for files in sorted(filenames): 
-        images.append(imageio.imread(os.path.join(screenshot_dir_path, files)))
-    imageio.mimwrite(gif_name, images,duration=duration)
-    return gif_name        
+
+    #Checking if files(images) exist in the directory.
+    if os.listdir(screenshot_dir_path) != []:
+        #Creating a GIF  
+        for files in sorted(filenames):
+            images.append(imageio.imread(os.path.join(screenshot_dir_path, files)))    
+        imageio.mimwrite(gif_name, images,duration=duration)
+        return gif_name        
