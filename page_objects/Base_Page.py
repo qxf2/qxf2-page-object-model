44b2008feec3dfd90e40aa9c1777e60c499c2a6c
nilaya1234 <nilaya@qxf2.com>
2020-01-24 13:38:32 +0530

    added import pytest to Base_Page

--- page_objects/Base_Page.py
+++ page_objects/Base_Page.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains 
-import unittest,time,logging,os,inspect,utils.Test_Rail
+import unittest,time,logging,os,inspect,utils.Test_Rail,pytest
 from utils.Base_Logging import Base_Logging
 from inspect import getargspec
 from utils.BrowserStack_Library import BrowserStack_Library
