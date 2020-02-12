5581ea3025c63beb913be046e7bff6fe1b80a467
Rajkumar M <rajkumar.m@qxf2.com>
2020-02-12 10:18:22 +0530

    Included a condition in Gif_maker.py file to fix Qxf2 GitHub issue 183

--- utils/Gif_Maker.py
+++ utils/Gif_Maker.py
@@ -10,6 +10,11 @@ def make_gif(screenshot_dir_path,name = "test_recap",suffix=".gif",duration=2):
     "Creates gif of the screenshots"
     gif_name = None
     images = []
+
+    #If the execution is from root directory using python command. The calling module name would include 'tests/test_---'. The below condition is used to avoid the additional directory name 'tests'
+    if "/" in name:
+        name=name.split("/")[-1]
+
     filenames = os.listdir(screenshot_dir_path)
     if len(filenames) != 0:
         gif_name = os.path.join(screenshot_dir_path, name + suffix)
