11cf66a58526dc080b40167e1f185231d8c80d9f
nilaya1234 <nilaya@qxf2.com>
2020-04-07 12:56:31 +0530

    . added

--- conftest.py
+++ conftest.py
@@ -283,7 +283,7 @@ def pytest_generate_tests(metafunc):
 
 
 def pytest_addoption(parser):
-    "Method to add the option to ini"
+    "Method to add the option to ini."
     parser.addini("rp_uuid",'help',type="pathlist")
     parser.addini("rp_endpoint",'help',type="pathlist")
     parser.addini("rp_project",'help',type="pathlist")
