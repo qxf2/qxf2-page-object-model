1a3cd8196160fe5cfd1c995444d4c06f4d6ecd27
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-05-11 15:27:23 +0530

    removed the space in the report file path argument

--- conftest.py
+++ conftest.py
@@ -249,10 +249,10 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
         #Initialize the Email_Pytest_Report object
         email_obj = Email_Pytest_Report()
         # Send html formatted email body message with pytest report as an attachment
-        email_obj.send_test_report_email(html_body_flag=True,attachment_flag=True,report_file_path= 'default')
+        email_obj.send_test_report_email(html_body_flag=True,attachment_flag=True,report_file_path='default')
     else:
         terminalreporter.config.getoption("--tesults").lower() == 'y'
-        Tesults.post_results_to_tesults()      
+        Tesults.post_results_to_tesults()     
 
 
 def pytest_generate_tests(metafunc):
