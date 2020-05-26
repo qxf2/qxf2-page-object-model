acaa150da797f57bf29e6a466fee6b814f111a6d
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-05-26 13:28:12 +0530

    review comments 1

--- utils/clean_up_repo.py
+++ utils/clean_up_repo.py
@@ -4,28 +4,31 @@ Run this file to delete all the example files and start fresh with your example.
 After this script runs, you will need to edit a few files to configure them to suit your own repository
 Usage: python clean_up_repo.py
 """
-import os,sys
+import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from conf import clean_up_repo_conf as conf
 
-def delete_file(filename):
-    # The method will delete a particular file
-    if os.path.exists(filename):
-        os.remove(filename)
-        print(f'{filename} deleted')
+def delete_file(file_name):
+    "The method will delete a particular file"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print(f'{file_name} deleted')
+
 
 def delete_files_in_dir(directory, files):
-    # The method will delete files in a particular directory
-    for filename in files:
-        delete_file(os.path.join(directory,filename))
+    "The method will delete files in a particular directory"
+    for file_name in files:
+        delete_file(os.path.join(directory, file_name))
+
 
 def delete_files_used_in_example():
-    # The method will delete files mentioned in the blog
-    # Blog URL: https://qxf2.com/blog/how-to-start-using-the-qxf2-framework-with-a-new-project/
-    for every_dir_list, every_file_list in zip(conf.dir_list,conf.file_list):
+    "The method will delete files mentioned in the blog"
+    "Blog URL: https://qxf2.com/blog/how-to-start-using-the-qxf2-framework-with-a-new-project/"
+    for every_dir_list, every_file_list in zip(conf.dir_list, conf.file_list):
         delete_files_in_dir(every_dir_list, every_file_list)
 
-#----START OF SCRIPT
+
+#---USAGE EXAMPLES
 if __name__ == "__main__":
     print("Running utility to delete the files")
     delete_files_used_in_example()
