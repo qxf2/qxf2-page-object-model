3bb11a7bec15965ae699d3464c6ebcc4372ab34f
shivaharip <shivahari@qxf2.com>
2020-09-17 12:06:18 +0530

    Commented out the sys path append step

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import os,pytest,sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+print(sys.path)
 from page_objects.PageFactory import PageFactory
 from conf import browser_os_name_conf
 from conf import base_url_conf
