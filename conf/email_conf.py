d729c613ebced3ba0c5e5fed3b20d615dad6cbba
Sravanti <sravanti.tatiraju@sciencelogic.com>
2020-11-09 19:05:30 +0530

    Revert "Fixed multiple emails generated issue"
    This reverts commit 97171f13a25acc96efb4c631b0711eeeb7082e6e.

--- conf/email_conf.py
+++ conf/email_conf.py
@@ -1,20 +1,20 @@
 #Details needed for the Gmail
 #Fill out the email details over here
 imaphost="imap.gmail.com"  #Add imap hostname of your email client
-username="sravanti.tatiraju@qxf2.com"
+username="Add your email address or username here"
 
 #Login has to use the app password because of Gmail security configuration
 # 1. Setup 2 factor authentication
 # 2. Follow the 2 factor authentication setup wizard to enable an app password
 #Src: https://support.google.com/accounts/answer/185839?hl=en
 #Src: https://support.google.com/mail/answer/185833?hl=en
-app_password="gffhkfmpnzhsnnqw"
+app_password="Add app password here"
 
 #Details for sending pytest report
 smtp_ssl_host = 'smtp.gmail.com'  # Add smtp ssl host of your email client
 smtp_ssl_port = 465  # Add smtp ssl port number of your email client
-sender = 'sravanti.tatiraju@qxf2.com' #Add senders email address here
-targets = ['sravanti.t25@gmail.com'] # Add recipients email address in a list
+sender = 'abc@xyz.com' #Add senders email address here
+targets = ['asd@xyz.com','qwe@xyz.com'] # Add recipients email address in a list
 
 
 
