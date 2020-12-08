f25b88051ce0602e1b0ee8b1850b27fad4222c41
sravantit25 <62924721+sravantit25@users.noreply.github.com>
2020-12-08 16:25:10 +0530

    Reverted changes to browser versions

--- conf/browser_os_name_conf.py
+++ conf/browser_os_name_conf.py
@@ -12,13 +12,13 @@ local_browsers = ["firefox","chrome"]  #local browser list against which tests w
 
 browsers = ["firefox","chrome","safari"]  #browsers to generate test run configuration to run on Browserstack/Sauce Labs
 firefox_versions = ["57","58"]  #firefox versions for the tests to run against on Browserstack/Sauce Labs
-chrome_versions = ["80","81"]  #chrome versions for the tests to run against on Browserstack/Sauce Labs
+chrome_versions = ["64","65"]  #chrome versions for the tests to run against on Browserstack/Sauce Labs
 safari_versions = ["8"]  #safari versions for the tests to run against on Browserstack/Sauce Labs
 os_list = ["windows","OS X"]   #list of os for the tests to run against on Browserstack/Sauce Labs
 windows_versions = ["8","10"]  #list of windows versions for the tests to run against on Browserstack/Sauce Labs
 os_x_versions = ["yosemite"]   #list of os x versions for the tests to run against on Browserstack/Sauce Labs
 sauce_labs_os_x_versions = ["10.10"] #Set if running on sauce_labs instead of "yosemite"
-default_config_list = [("chrome","81","windows","10")] #default configuration against which the test would run if no -B all option is used
+default_config_list = [("chrome","65","windows","10")] #default configuration against which the test would run if no -B all option is used
 
 
 def generate_configuration(browsers=browsers,firefox_versions=firefox_versions,chrome_versions=chrome_versions,safari_versions=safari_versions,
