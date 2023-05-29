22f2ff1f97902e7e61f01515938f1958edd2f36b
Archana GS <gsarchana93@gmail.com>
2023-05-29 12:17:54 +0530

    importing only password as env var

--- conf/testrailenv_conf.py
+++ conf/testrailenv_conf.py
@@ -4,5 +4,5 @@ Conf file to hold the TestRail url and credentials
 import os
 testrail_url = "Add your testrail url"
 testrail_user = 'TESTRAIL_USERNAME'
-testrail_password = os.environ('TESTRAIL_PASSWORD')
+testrail_password = os.environ.get('TESTRAIL_PASSWORD')
 
