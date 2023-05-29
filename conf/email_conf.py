16739f6ef3803b2b660019bf2bcd7712f87866d5
Archana GS <gsarchana93@gmail.com>
2023-05-29 12:10:42 +0530

    importing both username and password as env var

--- conf/email_conf.py
+++ conf/email_conf.py
@@ -1,15 +1,15 @@
 import os
 #Details needed for the Gmail
 #Fill out the email details over here
-imaphost="imap.gmail.com"  #Add imap hostname of your email client
-username="Add your email address or username here"
+imaphost ="imap.gmail.com"  #Add imap hostname of your email client
+username = os.environ['USERNAME']
 
 #Login has to use the app password because of Gmail security configuration
 # 1. Setup 2 factor authentication
 # 2. Follow the 2 factor authentication setup wizard to enable an app password
 #Src: https://support.google.com/accounts/answer/185839?hl=en
 #Src: https://support.google.com/mail/answer/185833?hl=en
-app_password = os.environ.get('APP_PASSWORD')
+app_password = os.environ['APP_PASSWORD']
 
 #Details for sending pytest report
 smtp_ssl_host = 'smtp.gmail.com'  # Add smtp ssl host of your email client
