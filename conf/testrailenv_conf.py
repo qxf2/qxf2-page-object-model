a6b44ab1c6ffef87b2f332d13fe89310b23590df
Archana GS <gsarchana93@gmail.com>
2023-05-29 12:12:54 +0530

    importing both username and password as env var

--- conf/testrailenv_conf.py
+++ conf/testrailenv_conf.py
@@ -3,6 +3,6 @@ Conf file to hold the TestRail url and credentials
 """
 import os
 testrail_url = "Add your testrail url"
-testrail_user = os.environ['TESTRAIL_USERNAME']
-testrail_password = os.environ['TESTRAIL_PASSWORD']
+testrail_user = os.environ.get['TESTRAIL_USERNAME']
+testrail_password = os.environ.get['TESTRAIL_PASSWORD']
 
