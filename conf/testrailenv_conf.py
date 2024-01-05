a2dd7ef19164220f9d09bc33d0a826512c16eb0b
Drishya <drishya.tm@qxf2.com>
2024-01-05 14:32:59 +0530

    added conf details to the env

--- conf/testrailenv_conf.py
+++
@@ -1,7 +0,0 @@
-"""
-Conf file to hold the TestRail url and credentials
-"""
-import os
-testrail_url = "Add your testrail url"
-testrail_user = os.environ.get('TESTRAIL_USERNAME')
-testrail_password = os.environ.get('TESTRAIL_PASSWORD')
