a6b52909e19409e33d05375b951a8ca097a46912
Ajitava Deb <97028279+ajitava-git@users.noreply.github.com>
2024-01-19 12:00:52 +0530

    Added accessibility to the page object model (#342)
    * Added accessibility to the page object model
    ---------
    
    Co-authored-by: Qxf2 Services <mak@qxf2.com>

--- page_objects/Base_Page.py
+++ page_objects/Base_Page.py
@@ -18,6 +18,8 @@ import conf.remote_credentials
 import conf.base_url_conf
 import conf.screenshot_conf
 from utils import Gif_Maker
+from utils import accessibility_util
+from utils import snapshot_util
 
 class Borg:
     #The borg design pattern is to share state
@@ -61,7 +63,8 @@ class Base_Page(Borg,unittest.TestCase):
         self.driver_obj = DriverFactory()
         if self.driver is not None:
             self.start() #Visit and initialize xpaths for the appropriate page
-
+            self.axe_util = accessibility_util.Accessibilityutil(self.driver)
+            self.snapshot_util = snapshot_util.Snapshotutil()
 
     def reset(self):
         "Reset the base page object"
@@ -478,6 +481,31 @@ class Base_Page(Borg,unittest.TestCase):
 
         return dom_elements
 
+
+    def accessibility_inject_axe(self):
+        "Inject Axe into the Page"
+        try:
+            return self.axe_util.inject()
+        except Exception as e:
+             self.write(e)
+
+    def accessibility_run_axe(self):
+        "Run Axe into the Page"
+        try:
+            return self.axe_util.run()
+        except Exception as e:
+             self.write(e)
+
+    def snapshot_assert_match(self, value, snapshot_name):
+        "Asserts the current value of the snapshot with the given snapshot_name"
+        result_flag = False
+        try:
+            self.snapshot_util.assert_match(value, snapshot_name)
+            result_flag = True
+        except Exception as e:
+             self.write(e)
+
+        return result_flag
 
     def click_element(self,locator,wait_time=3):
         "Click the button supplied"
