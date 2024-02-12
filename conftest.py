febc0e467cf4416be4efb911993f02435742e5d8
Drishya <drishya.tm@qxf2.com>
2024-02-12 21:52:16 +0530

    updated the conftest with removal of report portal conf file

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from page_objects.PageFactory import PageFactory
 from conf import browser_os_name_conf
 from conf import base_url_conf
-from conf import report_portal_conf
 from utils import post_test_reports_to_slack
 from utils.email_pytest_report import Email_Pytest_Report
 from endpoints.API_Player import API_Player
