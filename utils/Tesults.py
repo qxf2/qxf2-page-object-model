6df382a27f1935ece2ef3083782510d04270084c
Drishya <drishya.tm@qxf2.com>
2024-02-26 12:29:02 +0530

    dispaly message if env key is not set for tesults

--- utils/Tesults.py
+++ utils/Tesults.py
@@ -10,19 +10,24 @@ def post_results_to_tesults ():
     " This method is to post the results into the tesults"
     # uses default token unless otherwise specified
     token = os.getenv('tesults_target_token_default')
-    data = {
-        'target': token,
-        'results': { 'cases': cases }
-    }
-    print ('-----Tesults output-----')
-    if len(data['results']['cases']) > 0:
-        print (data)
-        print('Uploading results to Tesults...')
-        ret = tesults.results(data)
-        print ('success: ' + str(ret['success']))
-        print ('message: ' + str(ret['message']))
-        print ('warnings: ' + str(ret['warnings']))
-        print ('errors: ' + str(ret['errors']))
+    if token is None:
+        solution =("It looks like you are trying to use tesults to run your test."
+                   "Please make sure you have updated .env with the right credentials .")
+        print(f"\033[92m\nSOLUTION: {solution}\n\033[0m")
     else:
-        print ('No test results.')
+        data = {
+            'target': token,
+            'results': { 'cases': cases }
+        }
+        print ('-----Tesults output-----')
+        if len(data['results']['cases']) > 0:
+            print (data)
+            print('Uploading results to Tesults...')
+            ret = tesults.results(data)
+            print ('success: ' + str(ret['success']))
+            print ('message: ' + str(ret['message']))
+            print ('warnings: ' + str(ret['warnings']))
+            print ('errors: ' + str(ret['errors']))
+        else:
+            print ('No test results.')
         
