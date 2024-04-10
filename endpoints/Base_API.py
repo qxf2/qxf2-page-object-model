0c4a507fb114e506015b002c7bed4afc80025946
shivahariP <shivahari@qxf2.com>
2024-04-10 14:40:04 +0530

    Added generic method to make HTTP request against different HTTP methods

--- endpoints/Base_API.py
+++ endpoints/Base_API.py
@@ -8,6 +8,49 @@ from urllib.error import URLError
 class Base_API:
     "Main base class for Requests based scripts"
 
+    def make_request(self,
+                     method,
+                     url,
+                     headers={},
+                     auth=None,
+                     params=None,
+                     data=None,
+                     json=None):
+        "Get request"
+        json_response = None
+        error = {}
+        try:
+            response = self.request_obj.request(method=method,
+                                                url=url,
+                                                headers=headers,
+                                                auth=auth,
+                                                params=params,
+                                                data=data,
+                                                json=json)
+            try:
+                json_response = response.json()
+            except:
+                json_response = None
+        except (HTTPError,URLError) as e:
+            error = e
+            if isinstance(e,HTTPError):
+                error_message = e.read()
+                print("\n******\nGET Error: %s %s" %
+                    (url, error_message))
+            elif (e.reason.args[0] == 10061):
+                print("\033[1;31m\nURL open error: Please check if the API server is up or there is any other issue accessing the URL\033[1;m")
+                raise e
+            else:
+                print(e.reason.args)
+                # bubble error back up after printing relevant details
+                raise e # We raise error only when unknown errors occurs (other than HTTP error and url open error 10061)
+        except Exception as e:
+            print("Python says:%s" % str(e))
+            json_response = None
+
+        return {'response': response.status_code,'text':response.text,'json_response':json_response, 'error': error}
+
+
     def get(self, url, headers={}):
         "Get request"
         json_response = None
