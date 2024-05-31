aa7d299ce9c14efeba4836bccd8f2191c5c36ac6
akkuldn <akkul.dn@qxf2.com>
2024-06-01 02:18:40 +0530

    Better modularization of code

--- tests/test_weather_shopper_app.py
+++ tests/test_weather_shopper_app.py
@@ -15,141 +15,89 @@ from page_objects.PageFactory import PageFactory
 def test_weather_shopper_app(test_mobile_obj):
     "Run the test."
     try:
-        # Initalize flags for tests summary.
+        # Initialize flags for tests summary.
         expected_pass = 0
         actual_pass = -1
 
-        #1. Create a test object.
+        # Create a test object.
         test_mobile_obj = PageFactory.get_page_object("weather shopper app")
-
         start_time = int(time.time())
+
+        # Run test steps
+        #Get temperature from hompage
         temperature = test_mobile_obj.get_temperature()
 
-        if temperature < 19:
-            result_flag = test_mobile_obj.view_moisturizers()
+        #Visit the product page
+        product,result_flag = visit_product_page(test_mobile_obj, temperature)
+
+        if product == "Moisturizers":
             test_mobile_obj.log_result(result_flag,
-                positive="Successfully visited moistorizer page",
-                negative="Failed to visit moistorizer page",
-                level="critical")
-        elif temperature > 32:
-            result_flag = test_mobile_obj.view_sunscreens()
-            test_mobile_obj.log_result(result_flag,
-                positive="Successfully visited sunscreens page",
-                negative="Failed to visit sunscreens page",
-                level="critical")
+                                    positive="Successfully visited moisturizer page",
+                                    negative="Failed to visit moisturizer page",
+                                    level="critical")
         else:
-            skin_product = random.choice(['Moisturizers', 'Sunscreens'])
-            if skin_product == 'Moisturizers':
-                result_flag = test_mobile_obj.view_moisturizers()
-            else:
-                result_flag = test_mobile_obj.view_sunscreens()
-
             test_mobile_obj.log_result(result_flag,
-                positive="Successfully visited random page",
-                negative="Failed to visit random page",
-                level="critical")
+                                    positive="Successfully visited sunscreens page",
+                                    negative="Failed to visit sunscreens page",
+                                    level="critical")
 
-        #Get all products from page
-        all_items = test_mobile_obj.get_all_products()
+        #Get least and most expensive item from the page
+        least_expensive_item, most_expensive_item = get_items(test_mobile_obj)
 
-        #Calculate least and most expensive item
-        least_expensive_item = test_mobile_obj.get_least_expensive_item(all_items)
-        most_expensive_item = test_mobile_obj.get_most_expensive_item(all_items)
-
-        #Add least expensive item to cart
-        result_flag = test_mobile_obj.add_to_cart(least_expensive_item)
+        #Add items to cart
+        result_flag = add_items_to_cart(test_mobile_obj, least_expensive_item, most_expensive_item)
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully added least expensive item to cart",
-            negative="Failed to add least expensive item to cart",
-            level="critical")
-
-        #Add most expensive item to cart
-        result_flag = test_mobile_obj.add_to_cart(most_expensive_item)
-        test_mobile_obj.log_result(result_flag,
-            positive="Successfully added most expensive item to cart",
-            negative="Failed to add most expensive item to cart",
-            level="critical")
+                                    positive="Successfully added items item to cart",
+                                    negative="Failed to add one or mre items to the cart",
+                                    level="critical")
 
         #View cart
-        result_flag = test_mobile_obj.view_cart()
+        result_flag = view_cart(test_mobile_obj)
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully visited cart page",
-            negative="Failed to visit cart page",
-            level="critical")
+                                    positive="Successfully viewed cart",
+                                    negative="Failed to view cart",
+                                    level="critical")
 
         #Verify cart total
-        cart_total = test_mobile_obj.get_cart_total()
-        resul_flag = test_mobile_obj.verify_total(cart_total,
-            least_expensive_item['price'],
-            most_expensive_item['price'])
-
-        test_mobile_obj.log_result(resul_flag,
-            positive="Cart total is correct",
-            negative="Total is incorrect")
-
-        #Change quantity of least expensive item
-        quantity=2
-        result_flag = test_mobile_obj.change_quantity(least_expensive_item['name'],
-                    quantity=quantity)
+        result_flag = verify_cart(test_mobile_obj, least_expensive_item, most_expensive_item)
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully changed quantity of item",
-            negative="Failed to change quantity of item")
+                                positive="Cart total is correct",
+                                negative="Total is incorrect")
 
-        #Refresh cart total
-        result_flag = test_mobile_obj.refresh_total_amount()
+        #Change quantity of item in cart and verify cart total
+        quantity = 2
+        result_flag = change_quantity_and_verify(test_mobile_obj, least_expensive_item,
+                        most_expensive_item, quantity)
+
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully refreshed total",
-            negative="Failed to refresh total")
+                                positive="Successfully changed quantity of item",
+                                negative="Failed to change quantity of item")
 
-        #Verify cart total after change in quantity
-        cart_total_after_change = test_mobile_obj.get_cart_total()
-        resul_flag = test_mobile_obj.verify_total(cart_total_after_change,
-                least_expensive_item['price']*quantity,
-                most_expensive_item['price'])
-        test_mobile_obj.log_result(resul_flag,
-            positive="Total after changing quantity is accurate",
-            negative="Total after changing quantity is incorrect")
-
-        #Delete item from cart
-        result_flag = test_mobile_obj.delete_from_cart(most_expensive_item['name'])
+        #Delete item from cart and verify cart total
+        result_flag = delete_item_and_verify(test_mobile_obj, least_expensive_item,
+                        most_expensive_item, quantity)
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully deleted item from cart",
-            negative="Failed to delete item from cart")
+                            positive="Total after deletion is accurate",
+                            negative="Total after deletion is incorrect")
 
-        #Verify cart total after deletion
-        cart_total_after_deletion = test_mobile_obj.get_cart_total()
-        resul_flag = test_mobile_obj.verify_total(cart_total_after_deletion,
-                 least_expensive_item['price']*quantity)
-        test_mobile_obj.log_result(resul_flag,
-            positive="Total after deletion is accurate",
-            negative="Total after deletion is incorrect")
-
-        #Checkout
-        resul_flag = test_mobile_obj.checkout()
-        test_mobile_obj.log_result(resul_flag,
-            positive="Successfully entered checkout page",
-            negative="Failed to checkout",
-            level="critical")
-
-        #Enter valid payment details and submit payment
-        payment_details = {
-            "card_type": "Debit Card",
-            "email": "qxf2tester@example.com",
-            "card_number": "1234567890123456",
-            "card_expiry": "12/25",
-            "card_cvv": "123"
-        }
-        result_flag = test_mobile_obj.enter_valid_payment_details(payment_details)
+        #Checkout to payents page
+        result_flag = checkout(test_mobile_obj)
         test_mobile_obj.log_result(result_flag,
-            positive="Successfully completed payment",
-            negative="Failure to complete payment")
+                                    positive="Successfully entered checkout page",
+                                    negative="Failed to checkout",
+                                    level="critical")
 
+        #Verify payment
+        result_flag = verify_payment(test_mobile_obj)
+        test_mobile_obj.log_result(result_flag,
+                                positive="Successfully completed payment",
+                                negative="Failure to complete payment")
+
+        # Print out the results.
         test_mobile_obj.write(f'Script duration: {int(time.time() - start_time)} seconds\n')
-
-        #6. Print out the results.
         test_mobile_obj.write_test_summary()
 
-        #7. Teardown and Assertion.
+        # Teardown and Assertion.
         expected_pass = test_mobile_obj.result_counter
         actual_pass = test_mobile_obj.pass_counter
 
@@ -158,3 +106,123 @@ def test_weather_shopper_app(test_mobile_obj):
         print(f"Python says: {str(e)}")
 
     assert expected_pass == actual_pass, f"Test failed: {__file__}"
+
+def visit_product_page(test_mobile_obj, temperature):
+    "Visit the product page"
+    product = None
+    result_flag = False
+    if temperature < 19:
+        result_flag = test_mobile_obj.view_moisturizers()
+        product = "Moisturizers"
+
+    elif temperature > 32:
+        result_flag = test_mobile_obj.view_sunscreens()
+        product = "Sunscreens"
+
+    else:
+        skin_product = random.choice(['Moisturizers', 'Sunscreens'])
+        if skin_product == 'Moisturizers':
+            result_flag = test_mobile_obj.view_moisturizers()
+            product = "Moisturizers"
+        else:
+            result_flag = test_mobile_obj.view_sunscreens()
+            product = "Sunscreens"
+
+    return product, result_flag
+
+def get_items(test_mobile_obj):
+    "Get least and most expensive item from the page"
+
+    # Get all products from page
+    all_items = test_mobile_obj.get_all_products()
+
+    # Calculate least and most expensive item
+    least_expensive_item = test_mobile_obj.get_least_expensive_item(all_items)
+    most_expensive_item = test_mobile_obj.get_most_expensive_item(all_items)
+
+    return least_expensive_item, most_expensive_item
+
+def add_items_to_cart(test_mobile_obj, least_expensive_item, most_expensive_item):
+    "Add items to cart"
+    result_flag = test_mobile_obj.add_to_cart(least_expensive_item)
+
+    # Add most expensive item to cart
+    result_flag &= test_mobile_obj.add_to_cart(most_expensive_item)
+
+    return result_flag
+
+def view_cart(test_mobile_obj):
+    "View cart page"
+    result_flag = test_mobile_obj.view_cart()
+
+    return result_flag
+
+def verify_cart(test_mobile_obj, least_expensive_item, most_expensive_item):
+    "Verify cart total"
+    # Verify cart total
+    cart_total = test_mobile_obj.get_cart_total()
+    result_flag = test_mobile_obj.verify_total(cart_total,
+                                               least_expensive_item['price'],
+                                               most_expensive_item['price'])
+
+    return result_flag
+
+def change_quantity_and_verify(test_mobile_obj, least_expensive_item,
+                                most_expensive_item, quantity):
+    "Change quantity of item and verify cart total"
+    # Change quantity of least expensive item
+    result_flag = test_mobile_obj.change_quantity(least_expensive_item['name'], quantity=quantity)
+    test_mobile_obj.log_result(result_flag,
+                               positive="Successfully changed quantity of item",
+                               negative="Failed to change quantity of item",
+                               level = "critical")
+
+    # Refresh cart total
+    result_flag = test_mobile_obj.refresh_total_amount()
+    test_mobile_obj.log_result(result_flag,
+                               positive="Successfully refreshed total",
+                               negative="Failed to refresh total")
+
+    # Verify cart total after change in quantity
+    cart_total_after_change = test_mobile_obj.get_cart_total()
+    result_flag = test_mobile_obj.verify_total(cart_total_after_change,
+                                               least_expensive_item['price'] * quantity,
+                                               most_expensive_item['price'])
+
+    return result_flag
+
+def delete_item_and_verify(test_mobile_obj, least_expensive_item, most_expensive_item, quantity):
+    "Delete item from cart and verify cart total"
+    # Delete item from cart
+    result_flag = test_mobile_obj.delete_from_cart(most_expensive_item['name'])
+    test_mobile_obj.log_result(result_flag,
+                               positive="Successfully deleted item from cart",
+                               negative="Failed to delete item from cart",
+                               level = "critical")
+
+    # Verify cart total after deletion
+    cart_total_after_deletion = test_mobile_obj.get_cart_total()
+    result_flag = test_mobile_obj.verify_total(cart_total_after_deletion,
+                                               least_expensive_item['price'] * quantity)
+
+    return result_flag
+
+def checkout(test_mobile_obj):
+    "Checkout to payments page"
+    # Checkout
+    result_flag = test_mobile_obj.checkout()
+
+    return result_flag
+
+def verify_payment(test_mobile_obj):
+    "Submit payment details and verify payment"
+    # Enter valid payment details and submit payment
+    payment_details = {
+    "card_type": "Debit Card",
+    "email": "qxf2tester@example.com",
+    "card_number": "1234567890123456",
+    "card_expiry": "12/25",
+    "card_cvv": "123"
+    }
+    result_flag = test_mobile_obj.enter_valid_payment_details(payment_details)
+    return result_flag
