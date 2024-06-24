d961195e263bdf33208b32abfeb4f1ee5bd5e7db
avinash010 <avinash+ln@qxf2.com>
2024-06-24 16:28:02 +0530

    Moving payment to separate page

--- page_objects/weather_shopper.py
+++ page_objects/weather_shopper.py
@@ -6,6 +6,7 @@ import re
 import conf.locators_conf as locators
 from utils.Wrapit import Wrapit
 from .Mobile_Base_Page import Mobile_Base_Page
+from weather_shopper_payment import WeatherShopperPayment
 
 class WeatherShopper(Mobile_Base_Page):
     "Page object for Weathershopper application."
@@ -187,17 +188,3 @@ class WeatherShopper(Mobile_Base_Page):
         return result_flag
 
 
-    def enter_valid_payment_details(self, payment_details):
-        "This method is to enter valid payment details"
-
-        result_flag = self.click_element(locators.payment_method_dropdown)
-        result_flag &= self.click_element(locators.payment_card_type.format(payment_details["card_type"]))
-        result_flag &= self.set_text(locators.payment_email, payment_details["email"])
-        result_flag &= self.set_text(locators.payment_card_number, payment_details["card_number"])
-        result_flag &= self.set_text(locators.payment_card_expiry, payment_details["card_expiry"])
-        result_flag &= self.set_text(locators.payment_card_cvv, payment_details["card_cvv"])
-        result_flag &= self.click_element(locators.pay_button)
-        if self.get_element(locators.payment_success, verbose_flag=False) is None:
-            result_flag &= False
-
-        return result_flag
