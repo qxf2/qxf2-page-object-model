0a3f6fcfee123dafa2cb27f2b5af46c30ed451b3
Rohan Dudam <rohan@qxf2.com>
2024-07-04 16:53:40 +0530

    made reporting channels related changes

--- integrations/reporting_channels/email_pytest_report.py
+++ integrations/reporting_channels/email_pytest_report.py
@@ -8,22 +8,26 @@ To Do:
 
 Note:
 * We added subject, email body message as per our need. You can update that as per your requirement.
-* To generate html formatted test report, you need to use pytest-html plugin. To install it use command: pip install pytest-html
-* To generate pytest_report.html file use following command from the root of repo e.g. py.test --html = log/pytest_report.html
-* To generate pytest_report.log file use following command from the root of repo e.g. py.test -k example_form -r F -v > log/pytest_report.log
+* To generate html formatted test report, you need to use pytest-html plugin. 
+- To install it use command: pip install pytest-html
+* To generate pytest_report.html file use following command from the root of repo 
+- e.g. py.test --html = log/pytest_report.html
+* To generate pytest_report.log file use following command from the root of repo 
+- e.g. py.test -k example_form -r F -v > log/pytest_report.log
 """
+import os
+import sys
 import smtplib
-import os,sys
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.audio import MIMEAudio
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
-import mimetypes
 from email import encoders
+import mimetypes
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-class Email_Pytest_Report:
+class EmailPytestReport:
     "Class to email pytest report"
 
     def __init__(self):
@@ -36,18 +40,25 @@ class Email_Pytest_Report:
 
 
     def get_test_report_data(self,html_body_flag= True,report_file_path= 'default'):
-        "get test report data from pytest_report.html or pytest_report.txt or from user provided file"
+        '''get test report data from pytest_report.html or pytest_report.txt 
+        or from user provided file'''
         if html_body_flag == True and report_file_path == 'default':
-            #To generate pytest_report.html file use following command e.g. py.test --html = log/pytest_report.html
-            test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','log','pytest_report.html'))#Change report file name & address here
+            #To generate pytest_report.html file use following command
+            # e.g. py.test --html = log/pytest_report.html
+            #Change report file name & address here
+            test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                            '..','..','log','pytest_report.html'))
         elif html_body_flag == False and report_file_path == 'default':
-            #To generate pytest_report.log file add ">pytest_report.log" at end of py.test command e.g. py.test -k example_form -r F -v > log/pytest_report.log
-            test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','log','pytest_report.log'))#Change report file name & address here
+            #To generate pytest_report.log file add ">pytest_report.log" at end of py.test command
+            # e.g. py.test -k example_form -r F -v > log/pytest_report.log
+            #Change report file name & address here
+            test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                            '..','..','log','pytest_report.log'))
         else:
             test_report_file = report_file_path
         #check file exist or not
         if not os.path.exists(test_report_file):
-            raise Exception("File '%s' does not exist. Please provide valid file"%test_report_file)
+            raise Exception(f"File '{test_report_file}' does not exist. Please provide valid file")
 
         with open(test_report_file, "r") as in_file:
             testdata = ""
@@ -60,13 +71,16 @@ class Email_Pytest_Report:
     def get_attachment(self,attachment_file_path = 'default'):
         "Get attachment and attach it to mail"
         if attachment_file_path == 'default':
-            #To generate pytest_report.html file use following command e.g. py.test --html = log/pytest_report.html
-            attachment_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','log','pytest_report.html'))#Change report file name & address here
+            #To generate pytest_report.html file use following command
+            # e.g. py.test --html = log/pytest_report.html
+            #Change report file name & address here
+            attachment_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                    '..','..','log','pytest_report.html'))
         else:
             attachment_report_file = attachment_file_path
         #check file exist or not
         if not os.path.exists(attachment_report_file):
-            raise Exception("File '%s' does not exist. Please provide valid file"%attachment_report_file)
+            raise Exception(f"File '{attachment_report_file}' does not exist.")
 
         # Guess encoding type
         ctype, encoding = mimetypes.guess_type(attachment_report_file)
@@ -101,16 +115,21 @@ class Email_Pytest_Report:
         return attachment
 
 
-    def send_test_report_email(self,html_body_flag = True,attachment_flag = False,report_file_path = 'default'):
+    def send_test_report_email(self,html_body_flag = True,attachment_flag = False,
+                               report_file_path = 'default'):
         "send test report email"
-        #1. Get html formatted email body data from report_file_path file (log/pytest_report.html) and do not add it as an attachment
+        #1. Get html formatted email body data from report_file_path file (log/pytest_report.html)
+        # and do not add it as an attachment
         if html_body_flag == True and attachment_flag == False:
-            testdata = self.get_test_report_data(html_body_flag,report_file_path) #get html formatted test report data from log/pytest_report.html
+            #get html formatted test report data from log/pytest_report.html
+            testdata = self.get_test_report_data(html_body_flag,report_file_path)
             message = MIMEText(testdata,"html") # Add html formatted test data to email
 
-        #2. Get text formatted email body data from report_file_path file (log/pytest_report.log) and do not add it as an attachment
+        #2. Get text formatted email body data from report_file_path file (log/pytest_report.log)
+        # and do not add it as an attachment
         elif html_body_flag == False and attachment_flag == False:
-            testdata = self.get_test_report_data(html_body_flag,report_file_path) #get html test report data from log/pytest_report.log
+            #get html test report data from log/pytest_report.log
+            testdata = self.get_test_report_data(html_body_flag,report_file_path)
             message  = MIMEText(testdata) # Add text formatted test data to email
 
         #3. Add html formatted email body message along with an attachment file
@@ -120,7 +139,7 @@ class Email_Pytest_Report:
             html_body = MIMEText('''<p>Hello,</p>
                                      <p>&nbsp; &nbsp; &nbsp; &nbsp; Please check the attachment to see test built report.</p>
                                      <p><strong>Note: For best UI experience, download the attachment and open using Chrome browser.</strong></p>
-                                 ''',"html") # Add/Update email body message here as per your requirement
+                                 ''',"html") #Update email body message here as per your requirement
             message.attach(html_body)
             #add attachment to email
             attachment = self.get_attachment(report_file_path)
@@ -130,8 +149,9 @@ class Email_Pytest_Report:
         else:
             message = MIMEMultipart()
             #add test formatted body message to email
-            plain_text_body = MIMEText('''Hello,\n\tPlease check attachment to see test built report.
-                                       \n\nNote: For best UI experience, download the attachment and open  using Chrome browser.''')# Add/Update email body message here as per your requirement
+            plain_text_body = MIMEText('''Hello,\n\tPlease check attachment to see test report.
+                                       \n\nNote: For best UI experience, download the attachment and 
+                                       open using Chrome browser.''')#Update email body message here
             message.attach(plain_text_body)
             #add attachment to email
             attachment = self.get_attachment(report_file_path)
@@ -153,25 +173,33 @@ if __name__=='__main__':
     print("Start of %s"%__file__)
 
     #Initialize the Email_Pytest_Report object
-    email_obj = Email_Pytest_Report()
+    email_obj = EmailPytestReport()
     #1. Send html formatted email body message with pytest report as an attachment
-    #Here log/pytest_report.html is a default file. To generate pytest_report.html file use following command to the test e.g. py.test --html = log/pytest_report.html
-    email_obj.send_test_report_email(html_body_flag=True,attachment_flag=True,report_file_path= 'default')
+    # Here log/pytest_report.html is a default file.
+    # To generate pytest_report.html file use following command to the test
+    # e.g. py.test --html = log/pytest_report.html
+    email_obj.send_test_report_email(html_body_flag=True,attachment_flag=True,
+                                     report_file_path= 'default')
 
-    #Note: We commented below code to avoid sending multiple emails, you can try the other cases one by one to know more about email_pytest_report util.
+    #Note: We commented below code to avoid sending multiple emails,
+    # you can try the other cases one by one to know more about email_pytest_report util.
 
     '''
     #2. Send html formatted pytest report
-    email_obj.send_test_report_email(html_body_flag=True,attachment_flag=False,report_file_path= 'default')
+    email_obj.send_test_report_email(html_body_flag=True,attachment_flag=False,
+                                     report_file_path= 'default')
 
     #3. Send plain text formatted pytest report
-    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=False,report_file_path= 'default')
+    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=False,
+                                     report_file_path= 'default')
 
     #4. Send plain formatted email body message with pytest reports an attachment
-    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=True,report_file_path='default')
+    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=True,
+                                     report_file_path='default')
 
     #5. Send different type of attachment
-    image_file = ("C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg") # add attachment file here
-    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=True,report_file_path= image_file)
+    # add attachment file here below:
+    image_file = ("C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg")
+    email_obj.send_test_report_email(html_body_flag=False,attachment_flag=True,
+                                     report_file_path= image_file)
     '''
-
--- integrations/reporting_channels/post_test_reports_to_slack.py
+++ integrations/reporting_channels/post_test_reports_to_slack.py
@@ -1,46 +1,57 @@
 '''
-A Simple API util which used to post test reports on Slack Channel.
+A Simple script which used to post test reports on Slack Channel.
 
 Steps to Use:
-1. Generate Slack incoming webhook url by reffering our blog: https://qxf2.com/blog/post-pytest-test-results-on-slack/ & add url in our code
-2. Generate test report log file by adding ">log/pytest_report.log" command at end of py.test command for e.g. py.test -k example_form -I Y -r F -v > log/pytest_report.log
-Note: Your terminal must be pointed to root address of our POM while generating test report file using above command
+1. Generate Slack incoming webhook url by reffering our blog: 
+https://qxf2.com/blog/post-pytest-test-results-on-slack/ & add url in our code
+2. Generate test report log file by adding ">log/pytest_report.log" command 
+at end of pytest command 
+for e.g. py.test -k example_form -I Y -r F -v > log/pytest_report.log
+Note: Your terminal must be pointed to root address of our POM while generating 
+test report file using above command
 3. Check you are calling correct report log file or not
 '''
-import json,os,requests
+import os
+import json
+import requests
 
-def post_reports_to_slack():
-        #To generate incoming webhook url ref: https://qxf2.com/blog/post-pytest-test-results-on-slack/
-        url= "incoming webhook url"  #Add your Slack incoming webhook url here
+def post_reports_to_slack(timeout=30):
+    "Post report to Slack" 
+    #To generate incoming webhook url ref: https://qxf2.com/blog/post-pytest-test-results-on-slack/
+    url= os.getenv('slack_incoming_webhook_url')
 
-        #To generate pytest_report.log file add ">pytest_report.log" at end of py.test command for e.g. py.test -k example_form -I Y -r F -v > log/pytest_report.log
-        test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','log','pytest_report.log'))#Change report file name & address here
+    # To generate pytest_report.log file add ">pytest_report.log" at end of py.test command
+    # for e.g. py.test -k example_form --slack_flag y -v > log/pytest_report.log
+    # Update report file name & address here as per your requirement
+    test_report_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        '..','..','log','pytest_report.log'))
 
-        with open(test_report_file, "r") as in_file:
-                testdata = ""
-                for line in in_file:
-                        testdata = testdata + '\n' + line
+    with open(test_report_file, "r") as in_file:
+        testdata = ""
+        for line in in_file:
+            testdata = testdata + '\n' + line
 
-        # Set Slack Pass Fail bar indicator color according to test results
-        if 'FAILED' in testdata:
-            bar_color = "#ff0000"
-        else:
-            bar_color = "#36a64f"
+    # Set Slack Pass Fail bar indicator color according to test results
+    if 'FAILED' in testdata:
+        bar_color = "#ff0000"
+    else:
+        bar_color = "#36a64f"
 
-        data = {"attachments":[
-                            {"color": bar_color,
-                            "title": "Test Report",
-                            "text": testdata}
-                            ]}
-        json_params_encoded = json.dumps(data)
-        slack_response = requests.post(url=url,data=json_params_encoded,headers={"Content-type":"application/json"})
-        if slack_response.text == 'ok':
-                print('\n Successfully posted pytest report on Slack channel')
-        else:
-                print('\n Something went wrong. Unable to post pytest report on Slack channel. Slack Response:', slack_response)
-
+    data = {"attachments":[
+                        {"color": bar_color,
+                        "title": "Test Report",
+                        "text": testdata}
+                        ]}
+    json_params_encoded = json.dumps(data)
+    slack_response = requests.post(url=url,data=json_params_encoded,
+                                   headers={"Content-type":"application/json"},
+                                   timeout=timeout)
+    if slack_response.text == 'ok':
+        print('\n Successfully posted pytest report on Slack channel')
+    else:
+        print('\n Something went wrong. Unable to post pytest report on Slack channel.'
+              'Slack Response:', slack_response)
 
 #---USAGE EXAMPLES
 if __name__=='__main__':
-        post_reports_to_slack()
-
+    post_reports_to_slack()
