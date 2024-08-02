0a44389cff8f6b7e0e188323bb11867f28df37c0
akkuldn <akkul.dn@qxf2.com>
2024-08-02 19:59:47 +0530

    readded bitcoin test

--- page_objects/examples/weather_shopper_mobile_app/weather_shopper_payment_page.py
+++ page_objects/examples/weather_shopper_mobile_app/weather_shopper_payment_page.py
@@ -11,23 +11,6 @@ class WeatherShopperPaymentPage(Mobile_App_Helper):
 
     @Wrapit._exceptionHandler
     @Wrapit._screenshot
-    def enter_valid_payment_details(self, payment_details):
-        "This method is to enter valid payment details"
-
-        result_flag = self.click_element(locators.payment_method_dropdown)
-        result_flag &= self.click_element(locators.payment_card_type.format(payment_details["card_type"]))
-        result_flag &= self.set_text(locators.payment_email, payment_details["email"])
-        result_flag &= self.set_text(locators.payment_card_number, payment_details["card_number"])
-        result_flag &= self.set_text(locators.payment_card_expiry, payment_details["card_expiry"])
-        result_flag &= self.set_text(locators.payment_card_cvv, payment_details["card_cvv"])
-        result_flag &= self.click_element(locators.pay_button)
-        if self.get_element(locators.payment_success, verbose_flag=False) is None:
-            result_flag &= False
-
-        return result_flag
-
-    @Wrapit._exceptionHandler
-    @Wrapit._screenshot
     def select_payment_method(self, card_type):
         "Select the payment method"
         result_flag = self.click_element(locators.payment_method_dropdown)
