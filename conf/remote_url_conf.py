dad7ea22088beebaf9afdd50b0a383565ae87b54
Rohan Dudam <rohan@qxf2.com>
2024-08-14 12:04:41 +0530

    Updated BrowserStack api server url and endpoints to get correct session details

--- conf/remote_url_conf.py
+++ conf/remote_url_conf.py
@@ -2,6 +2,7 @@
 
 browserstack_url = "http://hub-cloud.browserstack.com/wd/hub"
 browserstack_app_upload_url = "https://api-cloud.browserstack.com/app-automate/upload"
+browserstack_api_server_url = "https://api.browserstack.com/automate/"
 saucelabs_url = "https://ondemand.eu-central-1.saucelabs.com:443/wd/hub"
 saucelabs_app_upload_url = 'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 lambdatest_url = "http://{}:{}@hub.lambdatest.com/wd/hub"
