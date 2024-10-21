3b21f1cfdfa4abe7d5efdf508a36f18b4c7940bb
mohanqxf2 <mohan@qxf2.com>
2024-10-21 18:56:08 +0530

    move load and save snapshot methods to utils

--- utils/snapshot_util.py
+++ utils/snapshot_util.py
@@ -2,7 +2,8 @@
 Snapshot Integration
 * This is a class which extends the methods of Snapshot parent class
 """
-
+import os
+import json
 import conf.snapshot_dir_conf
 from pytest_snapshot.plugin import Snapshot
 
@@ -14,3 +15,15 @@ class Snapshotutil(Snapshot):
                  allow_snapshot_deletion=False,
                  snapshot_dir=snapshot_dir):
         super().__init__(snapshot_update, allow_snapshot_deletion, snapshot_dir)
+
+    def load_snapshot(self, snapshot_file):
+        """Load the saved snapshot from a JSON file."""
+        if os.path.exists(snapshot_file):
+            with open(snapshot_file, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        return None
+
+    def save_snapshot(self, snapshot_file, violations):
+        """Save the given data as a snapshot in a JSON file."""
+        with open(snapshot_file, 'w', encoding='utf-8') as file:
+            json.dump(violations, file, ensure_ascii=False, indent=4)
