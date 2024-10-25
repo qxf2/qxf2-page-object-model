29922638eee340429912e37ed00a687f089208b2
Rohan Dudam <rohan@qxf2.com>
2024-10-25 18:07:56 +0530

    Moved gif_maker, made related changes, modified code to sort image file and pinned imageio version

--- core_helpers/gif_maker.py
+++ core_helpers/gif_maker.py
@@ -6,7 +6,7 @@ import imageio.v2 as imageio
 import os
 
 
-def make_gif(screenshot_dir_path,name = "test_recap",suffix=".gif",duration=2):
+def make_gif(screenshot_dir_path,name = "test_recap",suffix=".gif",frame_duration=0.5):
     "Creates gif of the screenshots"
     gif_name = None
     images = []
@@ -14,11 +14,11 @@ def make_gif(screenshot_dir_path,name = "test_recap",suffix=".gif",duration=2):
     if "/" in name:
         name=name.split("/")[-1]
 
-    filenames = os.listdir(screenshot_dir_path)
+    filenames = sorted(os.listdir(screenshot_dir_path))
     if len(filenames) != 0:
         gif_name = os.path.join(screenshot_dir_path, name + suffix)
         for files in sorted(filenames):
             images.append(imageio.imread(os.path.join(screenshot_dir_path, files)))
-        imageio.mimwrite(gif_name, images, duration=duration)
+        imageio.mimwrite(gif_name, images, duration=frame_duration)
 
     return gif_name
