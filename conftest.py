d7826e0ee74569580e8d03ac9a3f9c0b61c7acc6
shivahariP <shivahari@qxf2.com>
2025-02-06 14:05:30 +0530

    Fixed codacy issues

--- conftest.py
+++ conftest.py
@@ -49,7 +49,7 @@ def apitestconfig(config_factory):
 def mobiletestconfig(config_factory):
     "Mobile test config fixture"
     return config_factory.build_mobile_config()
- 
+
 # flake8: noqa: MC0001
 @pytest.fixture
 def test_obj(uitestconfig,
